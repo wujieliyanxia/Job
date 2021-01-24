fbe07ced7cd1b4c175023115dfd3463de4764218
jiewu <177825860@qq.com>
2021-01-24 21:35:01 +0800

    [job]delete unuse impore

--- job-core/src/main/java/com/buer/job/utils/CodeGenerator.java
+++ job-core/src/main/java/com/buer/job/utils/CodeGenerator.java
@@ -1,17 +1,13 @@
 package com.buer.job.utils;
 
 import com.baomidou.mybatisplus.core.exceptions.MybatisPlusException;
-import com.baomidou.mybatisplus.core.toolkit.StringPool;
 import com.baomidou.mybatisplus.generator.AutoGenerator;
 import com.baomidou.mybatisplus.generator.InjectionConfig;
 import com.baomidou.mybatisplus.generator.config.*;
-import com.baomidou.mybatisplus.generator.config.po.TableInfo;
 import com.baomidou.mybatisplus.generator.config.rules.NamingStrategy;
 import com.baomidou.mybatisplus.generator.engine.VelocityTemplateEngine;
 import org.apache.commons.lang3.StringUtils;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 /**
