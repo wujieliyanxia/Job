0f7969cf669e3e48ab7f25c1b910de5f361e600d
wu <jiewu@yangqianguan.com>
2021-03-30 20:47:19 +0800

    [job]改下编码

--- job-core/src/main/java/com/buer/job/model/entity/UserBehavior.java
+++ job-core/src/main/java/com/buer/job/model/entity/UserBehavior.java
@@ -1,15 +1,16 @@
 package com.buer.job.model.entity;
 
 import com.baomidou.mybatisplus.annotation.IdType;
-import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableField;
-import java.io.Serializable;
+import com.baomidou.mybatisplus.annotation.TableId;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
+import java.io.Serializable;
+
 /**
  * <p>
- * ?????
+ * 用户行为表
  * </p>
  *
  * @author jiewu
@@ -22,31 +23,31 @@ public class UserBehavior implements Serializable {
     private static final long serialVersionUID=1L;
 
     /**
-     * ??
+     * 主键
      */
     @TableId(value = "ID", type = IdType.AUTO)
     private Long id;
 
     /**
-     * ??id
+     * 用户id
      */
     @TableField("USER_ID")
     private Long userId;
 
     /**
-     * ??????,?????????????????
+     * 用户行为类别,收藏，浏览，报名、或者移除收藏等等
      */
     @TableField("TYPE")
     private String type;
 
     /**
-     * ?????id
+     * 对应的目标id
      */
     @TableField("TARGET_ID")
     private Long targetId;
 
     /**
-     * ????
+     * 创建时间
      */
     @TableField("TIME_CREATED")
     private Long timeCreated;
--- job-core/src/main/java/com/buer/job/model/mapper/UserBehaviorMapper.java
+++ job-core/src/main/java/com/buer/job/model/mapper/UserBehaviorMapper.java
@@ -5,7 +5,7 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 
 /**
  * <p>
- * ????? Mapper 接口
+ * 用户行为表 Mapper 接口
  * </p>
  *
  * @author jiewu
