d2d89b3d0d02e35a8be7fff6123cd16ed1dc9959
jiewu <1778258640@qq.com>
2021-07-13 22:27:33 +0800

    fix

--- job-api/src/main/java/com/buer/job/response/UserDetailResponse.java
+++ job-api/src/main/java/com/buer/job/response/UserDetailResponse.java
@@ -4,6 +4,7 @@ import com.buer.job.enums.Sex;
 import com.buer.job.enums.Timer;
 import com.buer.job.model.entity.UserAdditionalInfo;
 import com.buer.job.utils.Clock;
+import org.apache.commons.lang3.StringUtils;
 
 import java.util.TimeZone;
 
@@ -18,11 +19,15 @@ public class UserDetailResponse {
   public static UserDetailResponse from(UserAdditionalInfo userAdditionalInfo) {
     UserDetailResponse response = new UserDetailResponse();
     response.name = userAdditionalInfo.getName();
-    response.graduationTime = Clock.dateTimeStrFromTimestampPlusDays(userAdditionalInfo.getGraduationTime(), Timer.YYYY_MM, TimeZone.getDefault(), 0);
+    if(userAdditionalInfo.getGraduationTime()!=null){
+      response.graduationTime = Clock.dateTimeStrFromTimestampPlusDays(userAdditionalInfo.getGraduationTime(), Timer.YYYY_MM, TimeZone.getDefault(), 0);
+    }
     response.school = userAdditionalInfo.getSchool();
     response.nickName = userAdditionalInfo.getNickName();
     response.headPic = userAdditionalInfo.getHeaderPic();
-    response.sex = Sex.fromCode(userAdditionalInfo.getSex());
+    if(StringUtils.isNotBlank(userAdditionalInfo.getSex())){
+      response.sex = Sex.fromCode(userAdditionalInfo.getSex());
+    }
     return response;
   }
 }
